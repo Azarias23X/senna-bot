fad58ebb6594e15f3ae497899fcce44c3941b3a3
Azarias23X <93491154+Azarias23X@users.noreply.github.com>
2023-10-21 12:05:27 -0300

    Updated config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5491168352204']
+  ['5493573469639', 'Aza 2', true],
+  ['5493573469639']
 ] //Numeros de owner 
 
 global.mods = [''] 
